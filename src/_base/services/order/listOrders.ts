fe2482ea33b1509894654db13f60f6e89df9e0ec
Anderson Felix <andersonfelixmsouza@gmail.com>
2024-01-29 01:26:02 -0300

    Atualizar o listOrders.ts

--- src/_base/services/order/listOrders.ts
+++ src/_base/services/order/listOrders.ts
@@ -3,5 +3,8 @@ import { IOrder } from '@/_base/interfaces/order';
 
 export const listOrders = async (): Promise<IOrder[]> => {
   const { data } = await api.get(`/user/order/list`);
+
+  data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
   return data;
 };
