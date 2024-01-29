9db82b2214aa43570140fb99023bbc2442b006ec
Anderson Felix <andersonfelixmsouza@gmail.com>
2024-01-29 01:27:46 -0300

    chore: sort stores by created date

--- src/_base/services/store/listStores.ts
+++ src/_base/services/store/listStores.ts
@@ -3,5 +3,7 @@ import { IStore } from '@/_base/interfaces/store';
 
 export const listStores = async (): Promise<IStore[]> => {
   const { data } = await api.get(`/store/list`);
+  data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
   return data;
 };
